19fcf79ec71dc4271d7d0115139feaabab02f21b
TenneTCo <TenneTCo@users.noreply.github.com>
2018-10-20 12:49:26 +0100

    Update to Pos Year Reward scheme
    Update to Pos Year Reward scheme

--- src/main.h
+++ src/main.h
@@ -38,8 +38,8 @@ static const unsigned int MAX_INV_SZ = 50000;
 static const int64_t MIN_TX_FEE =  1000;
 static const int64_t MIN_RELAY_TX_FEE = MIN_TX_FEE;
 static const int64_t MAX_MONEY = 500000 * COIN;
-static const int64_t COIN_YEAR_REWARD = 200 * CENT;
-static const int64_t MAX_MINT_PROOF_OF_STAKE = 2.0 * COIN;  // 200% annual interest
+static const int64_t COIN_YEAR_REWARD = 2 * CENT;
+static const int64_t MAX_MINT_PROOF_OF_STAKE = 0.02 * COIN;  // 2% annual interest
 
 static const int MODIFIER_INTERVAL_SWITCH = 750;
 
